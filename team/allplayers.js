6225be8e911e1bc29166db0452e0381db0239670
joohopark-tot <jooho.park@oberoi-is.net>
2026-02-12 22:27:10 +0530

    Create allplayers.js

---
+++ team/allplayers.js
@@ -0,0 +1,46 @@
+import { arsenal } from "./team/arsenal.js";
+import { astonvilla } from "./team/astonvilla.js";
+import { bournemouth } from "./team/bournemouth.js";
+import { brentford } from "./team/brentford.js";
+import { brighton } from "./team/brighton.js";
+import { burnley } from "./team/burnley.js";
+import { chelsea } from "./team/chelsea.js";
+import { everton } from "./team/everton.js";
+import { fulham } from "./team/fulham.js";
+import { leeds } from "./team/leeds.js";
+import { liverpool } from "./team/liverpool.js";
+import { mancity } from "./team/mancity.js";
+import { manutd } from "./team/manutd.js";
+import { newcastle } from "./team/newcastle.js";
+import { nottingham } from "./team/nottingham.js";
+import { palace } from "./team/palace.js";
+import { sunderland } from "./team/sunderland.js";
+import { tottenham } from "./team/tottenham.js";
+import { westham } from "./team/westham.js";
+import { wolves } from "./team/wolves.js";
+
+
+/* ✅ replaces giant array */
+export default [
+  arsenal,
+  astonvilla,
+  bournemouth,
+  brentford,
+  brighton,
+  burnley,
+  chelsea,
+  everton,
+  fulham,
+  leeds,
+  liverpool,
+  mancity,
+  manutd,
+  newcastle,
+  nottingham,
+  palace,
+  sunderland,
+  tottenham,
+  westham,
+  wolves
+];
+
