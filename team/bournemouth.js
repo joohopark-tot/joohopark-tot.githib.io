9570ee2cf4b4cd0ce6c137ab6b33976ef7dafce2
joohopark-tot <jooho.park@oberoi-is.net>
2026-02-12 21:12:45 +0530

    Update bournemouth.js

--- team/bournemouth.js
+++ team/bournemouth.js
@@ -1,1 +1,37 @@
+const bournemouth = {
+  name: "Bournemouth",
+  players: [
+
+    { name:"Djordje Petrović", club:"Bournemouth", league:"Prem", nationality:"Serbia", position:"GK", age:26 },
+    { name:"Christos Mandas", club:"Bournemouth", league:"Prem", nationality:"Greece", position:"GK", age:24 },
+    { name:"Fraser Forster", club:"Bournemouth", league:"Prem", nationality:"England", position:"GK", age:37 },
+
+    { name:"Bafodé Diakité", club:"Bournemouth", league:"Prem", nationality:"France", position:"DF", age:25 },
+    { name:"Marcos Senesi", club:"Bournemouth", league:"Prem", nationality:"Argentina", position:"DF", age:28 },
+    { name:"Veljko Milosavljevic", club:"Bournemouth", league:"Prem", nationality:"Serbia", position:"DF", age:18 },
+    { name:"James Hill", club:"Bournemouth", league:"Prem", nationality:"England", position:"DF", age:24 },
+    { name:"Matai Akinmboni", club:"Bournemouth", league:"Prem", nationality:"United States", position:"DF", age:19 },
+    { name:"Adrien Truffert", club:"Bournemouth", league:"Prem", nationality:"France", position:"DF", age:24 },
+    { name:"Julio Soler", club:"Bournemouth", league:"Prem", nationality:"Argentina", position:"DF", age:20 },
+    { name:"Álex Jiménez", club:"Bournemouth", league:"Prem", nationality:"Spain", position:"DF", age:20 },
+    { name:"Adam Smith", club:"Bournemouth", league:"Prem", nationality:"England", position:"DF", age:34 },
+
+    { name:"Tyler Adams", club:"Bournemouth", league:"Prem", nationality:"United States", position:"MF", age:26 },
+    { name:"Alex Scott", club:"Bournemouth", league:"Prem", nationality:"England", position:"MF", age:22 },
+    { name:"Lewis Cook", club:"Bournemouth", league:"Prem", nationality:"England", position:"MF", age:29 },
+    { name:"Ryan Christie", club:"Bournemouth", league:"Prem", nationality:"Scotland", position:"MF", age:30 },
+    { name:"Alex Tóth", club:"Bournemouth", league:"Prem", nationality:"Hungary", position:"MF", age:20 },
+    { name:"Justin Kluivert", club:"Bournemouth", league:"Prem", nationality:"Netherlands", position:"MF", age:26 },
+    { name:"Marcus Tavernier", club:"Bournemouth", league:"Prem", nationality:"England", position:"MF", age:26 },
+
+    { name:"Amine Adli", club:"Bournemouth", league:"Prem", nationality:"Morocco", position:"FW", age:25 },
+    { name:"Ben Gannon-Doak", club:"Bournemouth", league:"Prem", nationality:"Scotland", position:"FW", age:20 },
+    { name:"David Brooks", club:"Bournemouth", league:"Prem", nationality:"Wales", position:"FW", age:28 },
+    { name:"Evanilson", club:"Bournemouth", league:"Prem", nationality:"Brazil", position:"FW", age:26 },
+    { name:"Rayan", club:"Bournemouth", league:"Prem", nationality:"Brazil", position:"FW", age:19 },
+    { name:"Junior Kroupi", club:"Bournemouth", league:"Prem", nationality:"France", position:"FW", age:19 },
+    { name:"Enes Ünal", club:"Bournemouth", league:"Prem", nationality:"Turkey", position:"FW", age:28 }
+
+  ]
+};
 
