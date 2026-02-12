99a6ab1a74ac11d433c0bc99b9e94f3ef3088efa
joohopark-tot <jooho.park@oberoi-is.net>
2026-02-12 21:03:01 +0530

    Update astonvilla.js

--- team/astonvilla.js
+++ team/astonvilla.js
@@ -1,1 +1,36 @@
+const astonvilla = {
+  name: "Aston Villa",
+  players: [
+
+    { name:"Emiliano Martínez", club:"Aston Villa", league:"Prem", nationality:"Argentina", position:"GK", age:33 },
+    { name:"Marco Bizot", club:"Aston Villa", league:"Prem", nationality:"Netherlands", position:"GK", age:34 },
+
+    { name:"Ezri Konsa", club:"Aston Villa", league:"Prem", nationality:"England", position:"DF", age:28 },
+    { name:"Pau Torres", club:"Aston Villa", league:"Prem", nationality:"Spain", position:"DF", age:29 },
+    { name:"Victor Lindelöf", club:"Aston Villa", league:"Prem", nationality:"Sweden", position:"DF", age:31 },
+    { name:"Tyrone Mings", club:"Aston Villa", league:"Prem", nationality:"England", position:"DF", age:32 },
+    { name:"Ian Maatsen", club:"Aston Villa", league:"Prem", nationality:"Netherlands", position:"DF", age:23 },
+    { name:"Lucas Digne", club:"Aston Villa", league:"Prem", nationality:"France", position:"DF", age:32 },
+    { name:"Matty Cash", club:"Aston Villa", league:"Prem", nationality:"Poland", position:"DF", age:28 },
+    { name:"Andrés García", club:"Aston Villa", league:"Prem", nationality:"Spain", position:"DF", age:23 },
+
+    { name:"Amadou Onana", club:"Aston Villa", league:"Prem", nationality:"Belgium", position:"MF", age:24 },
+    { name:"Boubacar Kamara", club:"Aston Villa", league:"Prem", nationality:"France", position:"MF", age:26 },
+    { name:"Lamare Bogarde", club:"Aston Villa", league:"Prem", nationality:"Netherlands", position:"MF", age:22 },
+    { name:"Youri Tielemans", club:"Aston Villa", league:"Prem", nationality:"Belgium", position:"MF", age:28 },
+    { name:"Douglas Luiz", club:"Aston Villa", league:"Prem", nationality:"Brazil", position:"MF", age:27 },
+    { name:"John McGinn", club:"Aston Villa", league:"Prem", nationality:"Scotland", position:"MF", age:31 },
+    { name:"Ross Barkley", club:"Aston Villa", league:"Prem", nationality:"England", position:"MF", age:32 },
+    { name:"Morgan Rogers", club:"Aston Villa", league:"Prem", nationality:"England", position:"MF", age:23 },
+    { name:"Harvey Elliott", club:"Aston Villa", league:"Prem", nationality:"England", position:"MF", age:22 },
+    { name:"Emiliano Buendía", club:"Aston Villa", league:"Prem", nationality:"Argentina", position:"MF", age:29 },
+
+    { name:"Jadon Sancho", club:"Aston Villa", league:"Prem", nationality:"England", position:"FW", age:25 },
+    { name:"Leon Bailey", club:"Aston Villa", league:"Prem", nationality:"Jamaica", position:"FW", age:28 },
+    { name:"Alysson", club:"Aston Villa", league:"Prem", nationality:"Brazil", position:"FW", age:19 },
+    { name:"Ollie Watkins", club:"Aston Villa", league:"Prem", nationality:"England", position:"FW", age:30 },
+    { name:"Tammy Abraham", club:"Aston Villa", league:"Prem", nationality:"England", position:"FW", age:28 }
+
+  ]
+};
 
