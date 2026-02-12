cd14646f2a3ef211f18ec898dafd687d948d9c5a
joohopark-tot <jooho.park@oberoi-is.net>
2026-02-12 11:04:20 +0530

    Create astonvilla.js

---
+++ team/astonvilla.js
@@ -0,0 +1,34 @@
+const astonVilla = {
+players: [
+
+/* --- GOALKEEPERS --- */
+{ name:"Emiliano Martínez", club:"Aston Villa", league:"Prem", nationality:"Argentina", position:"GK", age:33 },
+{ name:"Marco Bizot", club:"Aston Villa", league:"Prem", nationality:"Netherlands", position:"GK", age:34 },
+
+/* --- DEFENDERS --- */
+{ name:"Ezri Konsa", club:"Aston Villa", league:"Prem", nationality:"England", position:"DF", age:27 },
+{ name:"Pau Torres", club:"Aston Villa", league:"Prem", nationality:"Spain", position:"DF", age:28 },
+{ name:"Tyrone Mings", club:"Aston Villa", league:"Prem", nationality:"England", position:"DF", age:32 },
+{ name:"Matty Cash", club:"Aston Villa", league:"Prem", nationality:"Poland", position:"DF", age:28 },
+{ name:"Lucas Digne", club:"Aston Villa", league:"Prem", nationality:"France", position:"DF", age:32 },
+{ name:"Ian Maatsen", club:"Aston Villa", league:"Prem", nationality:"Netherlands", position:"DF", age:23 },
+{ name:"Diego Carlos", club:"Aston Villa", league:"Prem", nationality:"Brazil", position:"DF", age:32 },
+{ name:"Lamare Bogarde", club:"Aston Villa", league:"Prem", nationality:"Netherlands", position:"DF", age:21 },
+
+/* --- MIDFIELDERS --- */
+{ name:"Boubacar Kamara", club:"Aston Villa", league:"Prem", nationality:"France", position:"MF", age:25 },
+{ name:"Youri Tielemans", club:"Aston Villa", league:"Prem", nationality:"Belgium", position:"MF", age:28 },
+{ name:"John McGinn", club:"Aston Villa", league:"Prem", nationality:"Scotland", position:"MF", age:30 },
+{ name:"Amadou Onana", club:"Aston Villa", league:"Prem", nationality:"Belgium", position:"MF", age:24 },
+{ name:"Ross Barkley", club:"Aston Villa", league:"Prem", nationality:"England", position:"MF", age:31 },
+{ name:"Emiliano Buendía", club:"Aston Villa", league:"Prem", nationality:"Argentina", position:"MF", age:29 },
+{ name:"Morgan Rogers", club:"Aston Villa", league:"Prem", nationality:"England", position:"MF", age:23 },
+
+/* --- FORWARDS --- */
+{ name:"Ollie Watkins", club:"Aston Villa", league:"Prem", nationality:"England", position:"FW", age:30 },
+{ name:"Jhon Durán", club:"Aston Villa", league:"Prem", nationality:"Colombia", position:"FW", age:21 },
+{ name:"Donyell Malen", club:"Aston Villa", league:"Prem", nationality:"Netherlands", position:"FW", age:26 },
+{ name:"Evann Guessand", club:"Aston Villa", league:"Prem", nationality:"Ivory Coast", position:"FW", age:24 }
+
+]
+};
