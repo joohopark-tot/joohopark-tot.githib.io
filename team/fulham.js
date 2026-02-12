f35c04bb87bfe696926f6fb9c07efa45dd7ffda2
joohopark-tot <jooho.park@oberoi-is.net>
2026-02-12 21:38:48 +0530

    Update fulham.js

--- team/fulham.js
+++ team/fulham.js
@@ -1,1 +1,35 @@
+const fulham = {
+  name: "Fulham",
+  players: [
+
+    { name:"Bernd Leno", club:"Fulham", league:"Prem", nationality:"Germany", position:"GK", age:33 },
+    { name:"Benjamin Lecomte", club:"Fulham", league:"Prem", nationality:"France", position:"GK", age:34 },
+
+    { name:"Calvin Bassey", club:"Fulham", league:"Prem", nationality:"Nigeria", position:"DF", age:26 },
+    { name:"Joachim Andersen", club:"Fulham", league:"Prem", nationality:"Denmark", position:"DF", age:29 },
+    { name:"Issa Diop", club:"Fulham", league:"Prem", nationality:"France", position:"DF", age:29 },
+    { name:"Jorge Cuenca", club:"Fulham", league:"Prem", nationality:"Spain", position:"DF", age:26 },
+    { name:"Antonee Robinson", club:"Fulham", league:"Prem", nationality:"United States", position:"DF", age:28 },
+    { name:"Ryan Sessegnon", club:"Fulham", league:"Prem", nationality:"England", position:"DF", age:25 },
+    { name:"Kenny Tete", club:"Fulham", league:"Prem", nationality:"Netherlands", position:"DF", age:30 },
+    { name:"Timothy Castagne", club:"Fulham", league:"Prem", nationality:"Belgium", position:"DF", age:30 },
+
+    { name:"Sander Berge", club:"Fulham", league:"Prem", nationality:"Norway", position:"MF", age:27 },
+    { name:"Saša Lukić", club:"Fulham", league:"Prem", nationality:"Serbia", position:"MF", age:29 },
+    { name:"Harrison Reed", club:"Fulham", league:"Prem", nationality:"England", position:"MF", age:31 },
+    { name:"Tom Cairney", club:"Fulham", league:"Prem", nationality:"Scotland", position:"MF", age:35 },
+    { name:"Emile Smith Rowe", club:"Fulham", league:"Prem", nationality:"England", position:"MF", age:25 },
+    { name:"Josh King", club:"Fulham", league:"Prem", nationality:"England", position:"MF", age:19 },
+
+    { name:"Kevin", club:"Fulham", league:"Prem", nationality:"Brazil", position:"FW", age:23 },
+    { name:"Alex Iwobi", club:"Fulham", league:"Prem", nationality:"Nigeria", position:"FW", age:29 },
+    { name:"Oscar Bobb", club:"Fulham", league:"Prem", nationality:"Norway", position:"FW", age:22 },
+    { name:"Harry Wilson", club:"Fulham", league:"Prem", nationality:"Wales", position:"FW", age:28 },
+    { name:"Samuel Chukwueze", club:"Fulham", league:"Prem", nationality:"Nigeria", position:"FW", age:26 },
+    { name:"Rodrigo Muniz", club:"Fulham", league:"Prem", nationality:"Brazil", position:"FW", age:24 },
+    { name:"Raúl Jiménez", club:"Fulham", league:"Prem", nationality:"Mexico", position:"FW", age:34 },
+    { name:"Jonah Kusi-Asare", club:"Fulham", league:"Prem", nationality:"Sweden", position:"FW", age:18 }
+
+  ]
+};
 
