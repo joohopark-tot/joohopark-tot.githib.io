84331829a85a101ad16541e2dc49068fcf8e2282
joohopark-tot <jooho.park@oberoi-is.net>
2026-02-12 21:49:42 +0530

    Update westham.js

--- team/westham.js
+++ team/westham.js
@@ -1,1 +1,33 @@
+const westham = {
+  name: "West Ham",
+  players: [
+
+    { name:"Mads Hermansen", club:"West Ham", league:"Prem", nationality:"Denmark", position:"GK", age:25 },
+    { name:"Alphonse Areola", club:"West Ham", league:"Prem", nationality:"France", position:"GK", age:32 },
+    { name:"Lukasz Fabianski", club:"West Ham", league:"Prem", nationality:"Poland", position:"GK", age:40 },
+
+    { name:"Jean-Clair Todibo", club:"West Ham", league:"Prem", nationality:"France", position:"DF", age:26 },
+    { name:"Maximilian Kilman", club:"West Ham", league:"Prem", nationality:"England", position:"DF", age:28 },
+    { name:"Konstantinos Mavropanos", club:"West Ham", league:"Prem", nationality:"Greece", position:"DF", age:28 },
+    { name:"Ezra Mayers", club:"West Ham", league:"Prem", nationality:"England", position:"DF", age:19 },
+    { name:"El Hadji Malick Diouf", club:"West Ham", league:"Prem", nationality:"Senegal", position:"DF", age:21 },
+    { name:"Oliver Scarles", club:"West Ham", league:"Prem", nationality:"England", position:"DF", age:20 },
+    { name:"Aaron Wan-Bissaka", club:"West Ham", league:"Prem", nationality:"England", position:"DF", age:28 },
+    { name:"Kyle Walker-Peters", club:"West Ham", league:"Prem", nationality:"England", position:"DF", age:28 },
+
+    { name:"Soungoutou Magassa", club:"West Ham", league:"Prem", nationality:"France", position:"MF", age:22 },
+    { name:"Tomáš Souček", club:"West Ham", league:"Prem", nationality:"Czech Republic", position:"MF", age:30 },
+    { name:"Freddie Potts", club:"West Ham", league:"Prem", nationality:"England", position:"MF", age:22 },
+    { name:"Mateus Fernandes", club:"West Ham", league:"Prem", nationality:"Portugal", position:"MF", age:21 },
+
+    { name:"Crysencio Summerville", club:"West Ham", league:"Prem", nationality:"Netherlands", position:"FW", age:24 },
+    { name:"Keiber Lamadrid", club:"West Ham", league:"Prem", nationality:"Venezuela", position:"FW", age:22 },
+    { name:"Jarrod Bowen", club:"West Ham", league:"Prem", nationality:"England", position:"FW", age:29 },
+    { name:"Adama Traoré", club:"West Ham", league:"Prem", nationality:"Spain", position:"FW", age:30 },
+    { name:"Taty Castellanos", club:"West Ham", league:"Prem", nationality:"Argentina", position:"FW", age:27 },
+    { name:"Callum Wilson", club:"West Ham", league:"Prem", nationality:"England", position:"FW", age:33 },
+    { name:"Pablo", club:"West Ham", league:"Prem", nationality:"Brazil", position:"FW", age:22 }
+
+  ]
+};
 
