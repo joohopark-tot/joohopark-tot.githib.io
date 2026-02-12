bb428a56d0610bb87a5498b1c278d1b8d5dccdd7
joohopark-tot <jooho.park@oberoi-is.net>
2026-02-12 21:47:25 +0530

    Update palace.js

--- team/palace.js
+++ team/palace.js
@@ -1,1 +1,39 @@
+ const crystalPalace = {
+  name: "Crystal Palace",
+  players: [
+
+    { name:"Dean Henderson", club:"Crystal Palace", league:"Prem", nationality:"England", position:"GK", age:28 },
+    { name:"Walter Benítez", club:"Crystal Palace", league:"Prem", nationality:"Argentina", position:"GK", age:33 },
+    { name:"Remi Matthews", club:"Crystal Palace", league:"Prem", nationality:"England", position:"GK", age:32 },
+
+    { name:"Maxence Lacroix", club:"Crystal Palace", league:"Prem", nationality:"France", position:"DF", age:25 },
+    { name:"Chris Richards", club:"Crystal Palace", league:"Prem", nationality:"United States", position:"DF", age:25 },
+    { name:"Jaydee Canvot", club:"Crystal Palace", league:"Prem", nationality:"France", position:"DF", age:19 },
+    { name:"Chadi Riad", club:"Crystal Palace", league:"Prem", nationality:"Morocco", position:"DF", age:22 },
+    { name:"Tyrick Mitchell", club:"Crystal Palace", league:"Prem", nationality:"England", position:"DF", age:26 },
+    { name:"Borna Sosa", club:"Crystal Palace", league:"Prem", nationality:"Croatia", position:"DF", age:28 },
+    { name:"Rio Cardines", club:"Crystal Palace", league:"Prem", nationality:"Trinidad and Tobago", position:"DF", age:19 },
+    { name:"Daniel Muñoz", club:"Crystal Palace", league:"Prem", nationality:"Colombia", position:"DF", age:29 },
+    { name:"Caleb Kporha", club:"Crystal Palace", league:"Prem", nationality:"England", position:"DF", age:19 },
+    { name:"Nathaniel Clyne", club:"Crystal Palace", league:"Prem", nationality:"England", position:"DF", age:34 },
+
+    { name:"Adam Wharton", club:"Crystal Palace", league:"Prem", nationality:"England", position:"MF", age:22 },
+    { name:"Cheick Doucouré", club:"Crystal Palace", league:"Prem", nationality:"Mali", position:"MF", age:26 },
+    { name:"Jefferson Lerma", club:"Crystal Palace", league:"Prem", nationality:"Colombia", position:"MF", age:31 },
+    { name:"Kaden Rodney", club:"Crystal Palace", league:"Prem", nationality:"England", position:"MF", age:21 },
+    { name:"Will Hughes", club:"Crystal Palace", league:"Prem", nationality:"England", position:"MF", age:30 },
+    { name:"Daichi Kamada", club:"Crystal Palace", league:"Prem", nationality:"Japan", position:"MF", age:29 },
+    { name:"Justin Devenny", club:"Crystal Palace", league:"Prem", nationality:"Northern Ireland", position:"MF", age:22 },
+
+    { name:"Ismaïla Sarr", club:"Crystal Palace", league:"Prem", nationality:"Senegal", position:"FW", age:27 },
+    { name:"Yéremy Pino", club:"Crystal Palace", league:"Prem", nationality:"Spain", position:"FW", age:23 },
+    { name:"Brennan Johnson", club:"Crystal Palace", league:"Prem", nationality:"Wales", position:"FW", age:24 },
+    { name:"Evann Guessand", club:"Crystal Palace", league:"Prem", nationality:"Ivory Coast", position:"FW", age:24 },
+    { name:"Jean-Philippe Mateta", club:"Crystal Palace", league:"Prem", nationality:"France", position:"FW", age:28 },
+    { name:"Jørgen Strand Larsen", club:"Crystal Palace", league:"Prem", nationality:"Norway", position:"FW", age:26 },
+    { name:"Eddie Nketiah", club:"Crystal Palace", league:"Prem", nationality:"England", position:"FW", age:26 },
+    { name:"Christantus Uche", club:"Crystal Palace", league:"Prem", nationality:"Nigeria", position:"FW", age:22 }
+
+  ]
+};
 
