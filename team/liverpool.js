c65a71b8d4c29fdc8bff96e0cf6a34dd2b2d7713
joohopark-tot <jooho.park@oberoi-is.net>
2026-02-12 20:58:00 +0530

    Update liverpool.js

--- team/liverpool.js
+++ team/liverpool.js
@@ -1,1 +1,37 @@
+const liverpool = {
+  name: "Liverpool",
+  players: [
+
+    { name:"Giorgi Mamardashvili", club:"Liverpool", league:"Prem", nationality:"Georgia", position:"GK", age:25 },
+    { name:"Alisson", club:"Liverpool", league:"Prem", nationality:"Brazil", position:"GK", age:33 },
+    { name:"Freddie Woodman", club:"Liverpool", league:"Prem", nationality:"England", position:"GK", age:28 },
+
+    { name:"Ibrahima Konaté", club:"Liverpool", league:"Prem", nationality:"France", position:"DF", age:26 },
+    { name:"Giovanni Leoni", club:"Liverpool", league:"Prem", nationality:"Italy", position:"DF", age:19 },
+    { name:"Virgil van Dijk", club:"Liverpool", league:"Prem", nationality:"Netherlands", position:"DF", age:34 },
+    { name:"Joe Gomez", club:"Liverpool", league:"Prem", nationality:"England", position:"DF", age:28 },
+    { name:"Rhys Williams", club:"Liverpool", league:"Prem", nationality:"England", position:"DF", age:24 },
+    { name:"Milos Kerkez", club:"Liverpool", league:"Prem", nationality:"Hungary", position:"DF", age:22 },
+    { name:"Andrew Robertson", club:"Liverpool", league:"Prem", nationality:"Scotland", position:"DF", age:31 },
+    { name:"Jeremie Frimpong", club:"Liverpool", league:"Prem", nationality:"Netherlands", position:"DF", age:25 },
+    { name:"Conor Bradley", club:"Liverpool", league:"Prem", nationality:"Northern Ireland", position:"DF", age:22 },
+
+    { name:"Ryan Gravenberch", club:"Liverpool", league:"Prem", nationality:"Netherlands", position:"MF", age:23 },
+    { name:"Stefan Bajcetic", club:"Liverpool", league:"Prem", nationality:"Spain", position:"MF", age:21 },
+    { name:"Wataru Endo", club:"Liverpool", league:"Prem", nationality:"Japan", position:"MF", age:32 },
+    { name:"Alexis Mac Allister", club:"Liverpool", league:"Prem", nationality:"Argentina", position:"MF", age:27 },
+    { name:"Curtis Jones", club:"Liverpool", league:"Prem", nationality:"England", position:"MF", age:25 },
+    { name:"Trey Nyoni", club:"Liverpool", league:"Prem", nationality:"England", position:"MF", age:18 },
+    { name:"Florian Wirtz", club:"Liverpool", league:"Prem", nationality:"Germany", position:"MF", age:22 },
+    { name:"Dominik Szoboszlai", club:"Liverpool", league:"Prem", nationality:"Hungary", position:"MF", age:25 },
+
+    { name:"Cody Gakpo", club:"Liverpool", league:"Prem", nationality:"Netherlands", position:"FW", age:26 },
+    { name:"Rio Ngumoha", club:"Liverpool", league:"Prem", nationality:"England", position:"FW", age:17 },
+    { name:"Mohamed Salah", club:"Liverpool", league:"Prem", nationality:"Egypt", position:"FW", age:33 },
+    { name:"Federico Chiesa", club:"Liverpool", league:"Prem", nationality:"Italy", position:"FW", age:28 },
+    { name:"Alexander Isak", club:"Liverpool", league:"Prem", nationality:"Sweden", position:"FW", age:26 },
+    { name:"Hugo Ekitiké", club:"Liverpool", league:"Prem", nationality:"France", position:"FW", age:23 }
+
+  ]
+};
 
