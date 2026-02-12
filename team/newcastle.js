8ab226530d750e4a56d6e602fb4cacabbdc2fda9
joohopark-tot <jooho.park@oberoi-is.net>
2026-02-12 11:02:45 +0530

    Create newcastle.js

---
+++ team/newcastle.js
@@ -0,0 +1,41 @@
+/* --- NEWCASTLE UNITED 2025/26 --- */
+
+const newcastle = {
+  name: "Newcastle United",
+  players: [
+
+    // Goalkeepers
+    { name:"Nick Pope", club:"Newcastle", league:"Prem", nationality:"England", position:"GK", age:33 },
+    { name:"Aaron Ramsdale", club:"Newcastle", league:"Prem", nationality:"England", position:"GK", age:27 },
+    { name:"Mark Gillespie", club:"Newcastle", league:"Prem", nationality:"England", position:"GK", age:33 },
+
+    // Defenders
+    { name:"Kieran Trippier", club:"Newcastle", league:"Prem", nationality:"England", position:"DF", age:34 },
+    { name:"Sven Botman", club:"Newcastle", league:"Prem", nationality:"Netherlands", position:"DF", age:25 },
+    { name:"Fabian Schär", club:"Newcastle", league:"Prem", nationality:"Switzerland", position:"DF", age:33 },
+    { name:"Jamaal Lascelles", club:"Newcastle", league:"Prem", nationality:"England", position:"DF", age:31 },
+    { name:"Malick Thiaw", club:"Newcastle", league:"Prem", nationality:"Germany", position:"DF", age:24 },
+    { name:"Matt Targett", club:"Newcastle", league:"Prem", nationality:"England", position:"DF", age:29 },
+    { name:"Tino Livramento", club:"Newcastle", league:"Prem", nationality:"England", position:"DF", age:22 },
+    { name:"Dan Burn", club:"Newcastle", league:"Prem", nationality:"England", position:"DF", age:33 },
+    { name:"Lewis Hall", club:"Newcastle", league:"Prem", nationality:"England", position:"DF", age:21 },
+    { name:"Emil Krafth", club:"Newcastle", league:"Prem", nationality:"Sweden", position:"DF", age:31 },
+
+    // Midfielders
+    { name:"Bruno Guimarães", club:"Newcastle", league:"Prem", nationality:"Brazil", position:"MF", age:28 },
+    { name:"Sandro Tonali", club:"Newcastle", league:"Prem", nationality:"Italy", position:"MF", age:25 },
+    { name:"Joelinton", club:"Newcastle", league:"Prem", nationality:"Brazil", position:"MF", age:29 },
+    { name:"Joe Willock", club:"Newcastle", league:"Prem", nationality:"England", position:"MF", age:25 },
+    { name:"Lewis Miley", club:"Newcastle", league:"Prem", nationality:"England", position:"MF", age:19 },
+    { name:"Jacob Murphy", club:"Newcastle", league:"Prem", nationality:"England", position:"MF", age:30 },
+    { name:"Harvey Barnes", club:"Newcastle", league:"Prem", nationality:"England", position:"MF", age:27 },
+
+    // Forwards
+    { name:"Anthony Gordon", club:"Newcastle", league:"Prem", nationality:"England", position:"FW", age:24 },
+    { name:"Anthony Elanga", club:"Newcastle", league:"Prem", nationality:"Sweden", position:"FW", age:23 },
+    { name:"Yoane Wissa", club:"Newcastle", league:"Prem", nationality:"DR Congo", position:"FW", age:29 },
+    { name:"William Osula", club:"Newcastle", league:"Prem", nationality:"Denmark", position:"FW", age:21 },
+    { name:"Nick Woltemade", club:"Newcastle", league:"Prem", nationality:"Germany", position:"FW", age:23 }
+
+  ]
+};
