95dc21ee6abc2f41567c6527f991d51a24628d06
joohopark-tot <jooho.park@oberoi-is.net>
2026-02-12 21:04:25 +0530

    Update arsenal.js

--- team/arsenal.js
+++ team/arsenal.js
@@ -1,1 +1,35 @@
+const arsenal = {
+  name: "Arsenal",
+  players: [
+
+    { name:"David Raya", club:"Arsenal", league:"Prem", nationality:"Spain", position:"GK", age:30 },
+    { name:"Kepa Arrizabalaga", club:"Arsenal", league:"Prem", nationality:"Spain", position:"GK", age:31 },
+
+    { name:"William Saliba", club:"Arsenal", league:"Prem", nationality:"France", position:"DF", age:24 },
+    { name:"Gabriel Magalhães", club:"Arsenal", league:"Prem", nationality:"Brazil", position:"DF", age:28 },
+    { name:"Piero Hincapié", club:"Arsenal", league:"Prem", nationality:"Ecuador", position:"DF", age:24 },
+    { name:"Cristhian Mosquera", club:"Arsenal", league:"Prem", nationality:"Spain", position:"DF", age:21 },
+    { name:"Riccardo Calafiori", club:"Arsenal", league:"Prem", nationality:"Italy", position:"DF", age:23 },
+    { name:"Myles Lewis-Skelly", club:"Arsenal", league:"Prem", nationality:"England", position:"DF", age:19 },
+    { name:"Jurriën Timber", club:"Arsenal", league:"Prem", nationality:"Netherlands", position:"DF", age:24 },
+    { name:"Ben White", club:"Arsenal", league:"Prem", nationality:"England", position:"DF", age:28 },
+
+    { name:"Martín Zubimendi", club:"Arsenal", league:"Prem", nationality:"Spain", position:"MF", age:27 },
+    { name:"Christian Nørgaard", club:"Arsenal", league:"Prem", nationality:"Denmark", position:"MF", age:31 },
+    { name:"Declan Rice", club:"Arsenal", league:"Prem", nationality:"England", position:"MF", age:27 },
+    { name:"Mikel Merino", club:"Arsenal", league:"Prem", nationality:"Spain", position:"MF", age:29 },
+    { name:"Martin Ødegaard", club:"Arsenal", league:"Prem", nationality:"Norway", position:"MF", age:27 },
+    { name:"Eberechi Eze", club:"Arsenal", league:"Prem", nationality:"England", position:"MF", age:27 },
+
+    { name:"Gabriel Martinelli", club:"Arsenal", league:"Prem", nationality:"Brazil", position:"FW", age:24 },
+    { name:"Leandro Trossard", club:"Arsenal", league:"Prem", nationality:"Belgium", position:"FW", age:31 },
+    { name:"Bukayo Saka", club:"Arsenal", league:"Prem", nationality:"England", position:"FW", age:24 },
+    { name:"Noni Madueke", club:"Arsenal", league:"Prem", nationality:"England", position:"FW", age:23 },
+    { name:"Max Dowman", club:"Arsenal", league:"Prem", nationality:"England", position:"FW", age:16 },
+    { name:"Viktor Gyökeres", club:"Arsenal", league:"Prem", nationality:"Sweden", position:"FW", age:27 },
+    { name:"Kai Havertz", club:"Arsenal", league:"Prem", nationality:"Germany", position:"FW", age:26 },
+    { name:"Gabriel Jesus", club:"Arsenal", league:"Prem", nationality:"Brazil", position:"FW", age:27 }
+
+  ]
+};
 
