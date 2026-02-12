d41cc42c89a0a15ebfaa5e0ac87f08b4fd02870a
joohopark-tot <jooho.park@oberoi-is.net>
2026-02-12 21:25:00 +0530

    Update brighton.js

--- team/brighton.js
+++ team/brighton.js
@@ -1,1 +1,37 @@
+const brighton = {
+  name: "Brighton",
+  players: [
+
+    { name:"Bart Verbruggen", club:"Brighton", league:"Prem", nationality:"Netherlands", position:"GK", age:23 },
+    { name:"Jason Steele", club:"Brighton", league:"Prem", nationality:"England", position:"GK", age:35 },
+    { name:"Tom McGill", club:"Brighton", league:"Prem", nationality:"Canada", position:"GK", age:25 },
+
+    { name:"Jan Paul van Hecke", club:"Brighton", league:"Prem", nationality:"Netherlands", position:"DF", age:25 },
+    { name:"Olivier Boscagli", club:"Brighton", league:"Prem", nationality:"France", position:"DF", age:28 },
+    { name:"Igor Julio", club:"Brighton", league:"Prem", nationality:"Brazil", position:"DF", age:28 },
+    { name:"Adam Webster", club:"Brighton", league:"Prem", nationality:"England", position:"DF", age:31 },
+    { name:"Lewis Dunk", club:"Brighton", league:"Prem", nationality:"England", position:"DF", age:34 },
+    { name:"Ferdi Kadıoğlu", club:"Brighton", league:"Prem", nationality:"Türkiye", position:"DF", age:26 },
+    { name:"Maxim De Cuyper", club:"Brighton", league:"Prem", nationality:"Belgium", position:"DF", age:25 },
+    { name:"Joël Veltman", club:"Brighton", league:"Prem", nationality:"Netherlands", position:"DF", age:34 },
+
+    { name:"Carlos Baleba", club:"Brighton", league:"Prem", nationality:"Cameroon", position:"MF", age:22 },
+    { name:"Mats Wieffer", club:"Brighton", league:"Prem", nationality:"Netherlands", position:"MF", age:26 },
+    { name:"Jack Hinshelwood", club:"Brighton", league:"Prem", nationality:"England", position:"MF", age:20 },
+    { name:"Yasin Ayari", club:"Brighton", league:"Prem", nationality:"Sweden", position:"MF", age:22 },
+    { name:"Diego Gómez", club:"Brighton", league:"Prem", nationality:"Paraguay", position:"MF", age:22 },
+    { name:"Matt O'Riley", club:"Brighton", league:"Prem", nationality:"Denmark", position:"MF", age:25 },
+    { name:"Pascal Groß", club:"Brighton", league:"Prem", nationality:"Germany", position:"MF", age:34 },
+    { name:"James Milner", club:"Brighton", league:"Prem", nationality:"England", position:"MF", age:40 },
+
+    { name:"Kaoru Mitoma", club:"Brighton", league:"Prem", nationality:"Japan", position:"FW", age:28 },
+    { name:"Yankuba Minteh", club:"Brighton", league:"Prem", nationality:"The Gambia", position:"FW", age:21 },
+    { name:"Solly March", club:"Brighton", league:"Prem", nationality:"England", position:"FW", age:31 },
+    { name:"Georginio Rutter", club:"Brighton", league:"Prem", nationality:"France", position:"FW", age:23 },
+    { name:"Charalampos Kostoulas", club:"Brighton", league:"Prem", nationality:"Greece", position:"FW", age:18 },
+    { name:"Stefanos Tzimas", club:"Brighton", league:"Prem", nationality:"Greece", position:"FW", age:20 },
+    { name:"Danny Welbeck", club:"Brighton", league:"Prem", nationality:"England", position:"FW", age:35 }
+
+  ]
+};
 
