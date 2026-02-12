bd4508f4ef92af876a864fa6c4a87302e3aad021
joohopark-tot <jooho.park@oberoi-is.net>
2026-02-12 21:28:24 +0530

    Update burnley.js

--- team/burnley.js
+++ team/burnley.js
@@ -1,1 +1,36 @@
+const burnley = {
+  name: "Burnley",
+  players: [
+
+    { name:"Max Weiß", club:"Burnley", league:"Prem", nationality:"Germany", position:"GK", age:21 },
+    { name:"Martin Dúbravka", club:"Burnley", league:"Prem", nationality:"Slovakia", position:"GK", age:37 },
+    { name:"Václav Hladký", club:"Burnley", league:"Prem", nationality:"Czech Republic", position:"GK", age:35 },
+
+    { name:"Maxime Estève", club:"Burnley", league:"Prem", nationality:"France", position:"DF", age:23 },
+    { name:"Bashir Humphreys", club:"Burnley", league:"Prem", nationality:"England / Uganda", position:"DF", age:22 },
+    { name:"Hjalmar Ekdal", club:"Burnley", league:"Prem", nationality:"Sweden", position:"DF", age:27 },
+    { name:"Axel Tuanzebe", club:"Burnley", league:"Prem", nationality:"DR Congo / England", position:"DF", age:28 },
+    { name:"Joe Worrall", club:"Burnley", league:"Prem", nationality:"England", position:"DF", age:29 },
+    { name:"Jordan Beyer", club:"Burnley", league:"Prem", nationality:"Germany", position:"DF", age:25 },
+    { name:"Quilindschy Hartman", club:"Burnley", league:"Prem", nationality:"Netherlands / Curaçao", position:"DF", age:24 },
+    { name:"Lucas Pires", club:"Burnley", league:"Prem", nationality:"Brazil", position:"DF", age:24 },
+    { name:"Connor Roberts", club:"Burnley", league:"Prem", nationality:"Wales", position:"DF", age:30 },
+    { name:"Kyle Walker", club:"Burnley", league:"Prem", nationality:"England", position:"DF", age:35 },
+
+    { name:"Lesley Ugochukwu", club:"Burnley", league:"Prem", nationality:"France / Nigeria", position:"MF", age:21 },
+    { name:"Florentino", club:"Burnley", league:"Prem", nationality:"Portugal / Angola", position:"MF", age:26 },
+    { name:"Josh Cullen", club:"Burnley", league:"Prem", nationality:"Ireland / England", position:"MF", age:29 },
+    { name:"James Ward-Prowse", club:"Burnley", league:"Prem", nationality:"England", position:"MF", age:31 },
+    { name:"Josh Laurent", club:"Burnley", league:"Prem", nationality:"England", position:"MF", age:30 },
+    { name:"Hannibal", club:"Burnley", league:"Prem", nationality:"Tunisia / France", position:"MF", age:23 },
+    { name:"Mike Trésor", club:"Burnley", league:"Prem", nationality:"Belgium", position:"MF", age:26 },
+
+    { name:"Jaidon Anthony", club:"Burnley", league:"Prem", nationality:"England / Jamaica", position:"FW", age:26 },
+    { name:"Jacob Bruun Larsen", club:"Burnley", league:"Prem", nationality:"Denmark", position:"FW", age:27 },
+    { name:"Loum Tchaouna", club:"Burnley", league:"Prem", nationality:"France / Chad", position:"FW", age:22 },
+    { name:"Marcus Edwards", club:"Burnley", league:"Prem", nationality:"England / Cyprus", position:"FW", age:27 },
+    { name:"Lyle Foster", club:"Burnley", league:"Prem", nationality:"South Africa", position:"FW", age:25 },
+    { name:"Zian Flemming", club:"Burnley", league:"Prem", nationality:"Netherlands", position:"FW", age:27 },
+    { name:"Armando Broja", club:"Burnley", league:"Prem", nationality:"Albania / England", position:"FW", age:24 },
+    { name:"Zeki Amdouni", club:"Burnley", league:"Prem", nationality:"Switzerland / Tunisia", position:"FW",
 
