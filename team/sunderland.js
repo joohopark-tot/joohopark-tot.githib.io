7092ee6a6bda9df41f0731262f9d53038571afa4
joohopark-tot <jooho.park@oberoi-is.net>
2026-02-12 21:45:51 +0530

    Update sunderland.js

--- team/sunderland.js
+++ team/sunderland.js
@@ -1,1 +1,40 @@
+const sunderland = {
+  name: "Sunderland",
+  players: [
+
+    { name:"Robin Roefs", club:"Sunderland", league:"Prem", nationality:"Netherlands", position:"GK", age:23 },
+    { name:"Melker Ellborg", club:"Sunderland", league:"Prem", nationality:"Sweden", position:"GK", age:22 },
+    { name:"Simon Moore", club:"Sunderland", league:"Prem", nationality:"England", position:"GK", age:35 },
+
+    { name:"Dan Ballard", club:"Sunderland", league:"Prem", nationality:"Northern Ireland", position:"DF", age:26 },
+    { name:"Omar Alderete", club:"Sunderland", league:"Prem", nationality:"Paraguay", position:"DF", age:29 },
+    { name:"Luke O'Nien", club:"Sunderland", league:"Prem", nationality:"England", position:"DF", age:31 },
+    { name:"Dennis Cirkin", club:"Sunderland", league:"Prem", nationality:"England", position:"DF", age:23 },
+    { name:"Reinildo Mandava", club:"Sunderland", league:"Prem", nationality:"Mozambique", position:"DF", age:32 },
+    { name:"Lutsharel Geertruida", club:"Sunderland", league:"Prem", nationality:"Netherlands", position:"DF", age:25 },
+    { name:"Trai Hume", club:"Sunderland", league:"Prem", nationality:"Northern Ireland", position:"DF", age:23 },
+    { name:"Nordi Mukiele", club:"Sunderland", league:"Prem", nationality:"France", position:"DF", age:28 },
+
+    { name:"Granit Xhaka", club:"Sunderland", league:"Prem", nationality:"Switzerland", position:"MF", age:33 },
+    { name:"Habib Diarra", club:"Sunderland", league:"Prem", nationality:"Senegal", position:"MF", age:22 },
+    { name:"Noah Sadiki", club:"Sunderland", league:"Prem", nationality:"DR Congo", position:"MF", age:21 },
+    { name:"Enzo Le Fée", club:"Sunderland", league:"Prem", nationality:"France", position:"MF", age:26 },
+    { name:"Abdoullah Ba", club:"Sunderland", league:"Prem", nationality:"France", position:"MF", age:22 },
+    { name:"Chris Rigg", club:"Sunderland", league:"Prem", nationality:"England", position:"MF", age:18 },
+    { name:"Milan Aleksic", club:"Sunderland", league:"Prem", nationality:"Serbia", position:"MF", age:20 },
+    { name:"Harrison Jones", club:"Sunderland", league:"Prem", nationality:"England", position:"MF", age:21 },
+
+    { name:"Nilson Angulo", club:"Sunderland", league:"Prem", nationality:"Ecuador", position:"FW", age:22 },
+    { name:"Romaine Mundle", club:"Sunderland", league:"Prem", nationality:"England", position:"FW", age:22 },
+    { name:"Chemsdine Talbi", club:"Sunderland", league:"Prem", nationality:"Morocco", position:"FW", age:20 },
+    { name:"Bertrand Traoré", club:"Sunderland", league:"Prem", nationality:"Burkina Faso", position:"FW", age:30 },
+    { name:"Ian Poveda", club:"Sunderland", league:"Prem", nationality:"Colombia", position:"FW", age:26 },
+    { name:"Jocelin Ta Bi", club:"Sunderland", league:"Prem", nationality:"Ivory Coast", position:"FW", age:21 },
+    { name:"Brian Brobbey", club:"Sunderland", league:"Prem", nationality:"Netherlands", position:"FW", age:24 },
+    { name:"Eliezer Mayenda", club:"Sunderland", league:"Prem", nationality:"Spain", position:"FW", age:20 },
+    { name:"Wilson Isidor", club:"Sunderland", league:"Prem", nationality:"France", position:"FW", age:25 },
+    { name:"Ahmed Abdullahi", club:"Sunderland", league:"Prem", nationality:"Nigeria", position:"FW", age:21 }
+
+  ]
+};
 
