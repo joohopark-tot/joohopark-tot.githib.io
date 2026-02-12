653b124130e8d63de41c732dee6ee4e8b6200754
joohopark-tot <jooho.park@oberoi-is.net>
2026-02-12 20:55:20 +0530

    Update mancity.js

--- team/mancity.js
+++ team/mancity.js
@@ -1,1 +1,36 @@
+/* --- MANCHESTER CITY 2025/26 (Transfermarkt Verified) --- */
+
+const manCity = [
+
+{ name:"Gianluigi Donnarumma", club:"Man City", league:"Prem", nationality:"Italy", position:"GK", age:26 },
+{ name:"James Trafford", club:"Man City", league:"Prem", nationality:"England", position:"GK", age:23 },
+{ name:"Marcus Bettinelli", club:"Man City", league:"Prem", nationality:"England", position:"GK", age:33 },
+
+{ name:"Joško Gvardiol", club:"Man City", league:"Prem", nationality:"Croatia", position:"DF", age:24 },
+{ name:"Rúben Dias", club:"Man City", league:"Prem", nationality:"Portugal", position:"DF", age:28 },
+{ name:"Marc Guéhi", club:"Man City", league:"Prem", nationality:"England", position:"DF", age:25 },
+{ name:"Abdukodir Khusanov", club:"Man City", league:"Prem", nationality:"Uzbekistan", position:"DF", age:21 },
+{ name:"John Stones", club:"Man City", league:"Prem", nationality:"England", position:"DF", age:31 },
+{ name:"Nathan Aké", club:"Man City", league:"Prem", nationality:"Netherlands", position:"DF", age:30 },
+{ name:"Rayan Aït-Nouri", club:"Man City", league:"Prem", nationality:"Algeria", position:"DF", age:24 },
+{ name:"Nico O'Reilly", club:"Man City", league:"Prem", nationality:"England", position:"DF", age:20 },
+{ name:"Matheus Nunes", club:"Man City", league:"Prem", nationality:"Portugal", position:"DF", age:27 },
+{ name:"Rico Lewis", club:"Man City", league:"Prem", nationality:"England", position:"DF", age:21 },
+
+{ name:"Rodri", club:"Man City", league:"Prem", nationality:"Spain", position:"MF", age:29 },
+{ name:"Nico González", club:"Man City", league:"Prem", nationality:"Spain", position:"MF", age:24 },
+{ name:"Tijjani Reijnders", club:"Man City", league:"Prem", nationality:"Netherlands", position:"MF", age:27 },
+{ name:"Mateo Kovačić", club:"Man City", league:"Prem", nationality:"Croatia", position:"MF", age:31 },
+{ name:"Sverre Nypan", club:"Man City", league:"Prem", nationality:"Norway", position:"MF", age:19 },
+{ name:"Phil Foden", club:"Man City", league:"Prem", nationality:"England", position:"MF", age:25 },
+{ name:"Rayan Cherki", club:"Man City", league:"Prem", nationality:"France", position:"MF", age:22 },
+{ name:"Bernardo Silva", club:"Man City", league:"Prem", nationality:"Portugal", position:"MF", age:31 },
+
+{ name:"Jérémy Doku", club:"Man City", league:"Prem", nationality:"Belgium", position:"FW", age:23 },
+{ name:"Savinho", club:"Man City", league:"Prem", nationality:"Brazil", position:"FW", age:21 },
+{ name:"Antoine Semenyo", club:"Man City", league:"Prem", nationality:"Ghana", position:"FW", age:26 },
+{ name:"Erling Haaland", club:"Man City", league:"Prem", nationality:"Norway", position:"FW", age:25 },
+{ name:"Omar Marmoush", club:"Man City", league:"Prem", nationality:"Egypt", position:"FW", age:27 }
+
+];
 
