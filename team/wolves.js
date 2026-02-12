fde267d5bf4198841a432e21d7621edfe3fdf24d
joohopark-tot <jooho.park@oberoi-is.net>
2026-02-12 21:51:35 +0530

    Update wolves.js

--- team/wolves.js
+++ team/wolves.js
@@ -1,1 +1,34 @@
+const wolves = {
+  name: "Wolves",
+  players: [
+
+    { name:"José Sá", club:"Wolves", league:"Prem", nationality:"Portugal", position:"GK", age:33 },
+    { name:"Sam Johnstone", club:"Wolves", league:"Prem", nationality:"England", position:"GK", age:32 },
+    { name:"Daniel Bentley", club:"Wolves", league:"Prem", nationality:"England", position:"GK", age:32 },
+
+    { name:"Toti", club:"Wolves", league:"Prem", nationality:"Portugal", position:"DF", age:27 },
+    { name:"Ladislav Krejci", club:"Wolves", league:"Prem", nationality:"Czech Republic", position:"DF", age:26 },
+    { name:"Santiago Bueno", club:"Wolves", league:"Prem", nationality:"Uruguay", position:"DF", age:27 },
+    { name:"Yerson Mosquera", club:"Wolves", league:"Prem", nationality:"Colombia", position:"DF", age:24 },
+    { name:"Hugo Bueno", club:"Wolves", league:"Prem", nationality:"Spain", position:"DF", age:23 },
+    { name:"David Møller Wolfe", club:"Wolves", league:"Prem", nationality:"Norway", position:"DF", age:23 },
+    { name:"Jackson Tchatchoua", club:"Wolves", league:"Prem", nationality:"Cameroon", position:"DF", age:24 },
+    { name:"Pedro Lima", club:"Wolves", league:"Prem", nationality:"Brazil", position:"DF", age:19 },
+    { name:"Matt Doherty", club:"Wolves", league:"Prem", nationality:"Ireland", position:"DF", age:34 },
+
+    { name:"André", club:"Wolves", league:"Prem", nationality:"Brazil", position:"MF", age:24 },
+    { name:"João Gomes", club:"Wolves", league:"Prem", nationality:"Brazil", position:"MF", age:25 },
+    { name:"Jean-Ricner Bellegarde", club:"Wolves", league:"Prem", nationality:"Haiti", position:"MF", age:27 },
+    { name:"Rodrigo Gomes", club:"Wolves", league:"Prem", nationality:"Portugal", position:"MF", age:22 },
+    { name:"Angel Gomes", club:"Wolves", league:"Prem", nationality:"England", position:"MF", age:25 },
+    { name:"Mateus Mané", club:"Wolves", league:"Prem", nationality:"England", position:"MF", age:18 },
+
+    { name:"Enso González", club:"Wolves", league:"Prem", nationality:"Paraguay", position:"FW", age:21 },
+    { name:"Tolu Arokodare", club:"Wolves", league:"Prem", nationality:"Nigeria", position:"FW", age:25 },
+    { name:"Hee-chan Hwang", club:"Wolves", league:"Prem", nationality:"South Korea", position:"FW", age:30 },
+    { name:"Adam Armstrong", club:"Wolves", league:"Prem", nationality:"England", position:"FW", age:29 },
+    { name:"Nathan Fraser", club:"Wolves", league:"Prem", nationality:"Ireland", position:"FW", age:20 }
+
+  ]
+};
 
