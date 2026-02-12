4a18f45f75feb386fc6629e9f737e7190b75e9a4
joohopark-tot <jooho.park@oberoi-is.net>
2026-02-12 20:52:52 +0530

    Update manutd.js

--- team/manutd.js
+++ team/manutd.js
@@ -1,1 +1,36 @@
+/* --- MANCHESTER UNITED 2025/26 (Transfermarkt Verified) --- */
+
+const manUnited = [
+
+{ name:"Senne Lammens", club:"Man Utd", league:"Prem", nationality:"Belgium", position:"GK", age:23 },
+{ name:"Altay Bayındır", club:"Man Utd", league:"Prem", nationality:"Turkey", position:"GK", age:27 },
+{ name:"Tom Heaton", club:"Man Utd", league:"Prem", nationality:"England", position:"GK", age:39 },
+
+{ name:"Leny Yoro", club:"Man Utd", league:"Prem", nationality:"France", position:"DF", age:20 },
+{ name:"Matthijs de Ligt", club:"Man Utd", league:"Prem", nationality:"Netherlands", position:"DF", age:26 },
+{ name:"Lisandro Martínez", club:"Man Utd", league:"Prem", nationality:"Argentina", position:"DF", age:28 },
+{ name:"Harry Maguire", club:"Man Utd", league:"Prem", nationality:"England", position:"DF", age:32 },
+{ name:"Ayden Heaven", club:"Man Utd", league:"Prem", nationality:"England", position:"DF", age:19 },
+{ name:"Tyler Fredricson", club:"Man Utd", league:"Prem", nationality:"England", position:"DF", age:20 },
+{ name:"Patrick Dorgu", club:"Man Utd", league:"Prem", nationality:"Denmark", position:"DF", age:21 },
+{ name:"Luke Shaw", club:"Man Utd", league:"Prem", nationality:"England", position:"DF", age:30 },
+{ name:"Tyrell Malacia", club:"Man Utd", league:"Prem", nationality:"Netherlands", position:"DF", age:26 },
+{ name:"Diego León", club:"Man Utd", league:"Prem", nationality:"Paraguay", position:"DF", age:18 },
+{ name:"Diogo Dalot", club:"Man Utd", league:"Prem", nationality:"Portugal", position:"DF", age:26 },
+{ name:"Noussair Mazraoui", club:"Man Utd", league:"Prem", nationality:"Morocco", position:"DF", age:28 },
+
+{ name:"Manuel Ugarte", club:"Man Utd", league:"Prem", nationality:"Uruguay", position:"MF", age:24 },
+{ name:"Casemiro", club:"Man Utd", league:"Prem", nationality:"Brazil", position:"MF", age:33 },
+{ name:"Kobbie Mainoo", club:"Man Utd", league:"Prem", nationality:"England", position:"MF", age:20 },
+{ name:"Bruno Fernandes", club:"Man Utd", league:"Prem", nationality:"Portugal", position:"MF", age:31 },
+{ name:"Mason Mount", club:"Man Utd", league:"Prem", nationality:"England", position:"MF", age:27 },
+
+{ name:"Amad Diallo", club:"Man Utd", league:"Prem", nationality:"Ivory Coast", position:"FW", age:23 },
+{ name:"Bryan Mbeumo", club:"Man Utd", league:"Prem", nationality:"Cameroon", position:"FW", age:26 },
+{ name:"Matheus Cunha", club:"Man Utd", league:"Prem", nationality:"Brazil", position:"FW", age:26 },
+{ name:"Benjamin Šeško", club:"Man Utd", league:"Prem", nationality:"Slovenia", position:"FW", age:22 },
+{ name:"Joshua Zirkzee", club:"Man Utd", league:"Prem", nationality:"Netherlands", position:"FW", age:24 },
+{ name:"Chido Obi", club:"Man Utd", league:"Prem", nationality:"Denmark", position:"FW", age:17 }
+
+];
 
