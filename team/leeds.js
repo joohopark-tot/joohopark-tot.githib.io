4e1c4baf8deea8e6cf1ed7c3f1a7d8230825c035
joohopark-tot <jooho.park@oberoi-is.net>
2026-02-12 21:41:28 +0530

    Update leeds.js

--- team/leeds.js
+++ team/leeds.js
@@ -1,1 +1,37 @@
+const leeds = {
+  name: "Leeds",
+  players: [
+
+    { name:"Lucas Perri", club:"Leeds", league:"Prem", nationality:"Brazil", position:"GK", age:28 },
+    { name:"Illan Meslier", club:"Leeds", league:"Prem", nationality:"France", position:"GK", age:25 },
+    { name:"Karl Darlow", club:"Leeds", league:"Prem", nationality:"Wales", position:"GK", age:35 },
+    { name:"Alex Cairns", club:"Leeds", league:"Prem", nationality:"England", position:"GK", age:33 },
+
+    { name:"Pascal Struijk", club:"Leeds", league:"Prem", nationality:"Netherlands", position:"DF", age:26 },
+    { name:"Jaka Bijol", club:"Leeds", league:"Prem", nationality:"Slovenia", position:"DF", age:27 },
+    { name:"Joe Rodon", club:"Leeds", league:"Prem", nationality:"Wales", position:"DF", age:28 },
+    { name:"Sebastiaan Bornauw", club:"Leeds", league:"Prem", nationality:"Belgium", position:"DF", age:26 },
+    { name:"Gabriel Gudmundsson", club:"Leeds", league:"Prem", nationality:"Sweden", position:"DF", age:26 },
+    { name:"Sam Byram", club:"Leeds", league:"Prem", nationality:"England", position:"DF", age:32 },
+    { name:"James Justin", club:"Leeds", league:"Prem", nationality:"England", position:"DF", age:27 },
+    { name:"Jayden Bogle", club:"Leeds", league:"Prem", nationality:"England", position:"DF", age:25 },
+
+    { name:"Ethan Ampadu", club:"Leeds", league:"Prem", nationality:"Wales", position:"MF", age:25 },
+    { name:"Anton Stach", club:"Leeds", league:"Prem", nationality:"Germany", position:"MF", age:27 },
+    { name:"Ilia Gruev", club:"Leeds", league:"Prem", nationality:"Bulgaria", position:"MF", age:25 },
+    { name:"Charlie Crew", club:"Leeds", league:"Prem", nationality:"Wales", position:"MF", age:19 },
+    { name:"Sean Longstaff", club:"Leeds", league:"Prem", nationality:"England", position:"MF", age:28 },
+    { name:"Ao Tanaka", club:"Leeds", league:"Prem", nationality:"Japan", position:"MF", age:27 },
+    { name:"Facundo Buonanotte", club:"Leeds", league:"Prem", nationality:"Argentina", position:"MF", age:21 },
+    { name:"Brenden Aaronson", club:"Leeds", league:"Prem", nationality:"United States", position:"MF", age:25 },
+
+    { name:"Noah Okafor", club:"Leeds", league:"Prem", nationality:"Switzerland", position:"FW", age:25 },
+    { name:"Wilfried Gnonto", club:"Leeds", league:"Prem", nationality:"Italy", position:"FW", age:22 },
+    { name:"Daniel James", club:"Leeds", league:"Prem", nationality:"Wales", position:"FW", age:28 },
+    { name:"Dominic Calvert-Lewin", club:"Leeds", league:"Prem", nationality:"England", position:"FW", age:28 },
+    { name:"Joël Piroe", club:"Leeds", league:"Prem", nationality:"Suriname", position:"FW", age:26 },
+    { name:"Lukas Nmecha", club:"Leeds", league:"Prem", nationality:"Germany", position:"FW", age:27 }
+
+  ]
+};
 
