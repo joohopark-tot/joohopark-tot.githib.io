58e27196ef36386f71e2d2264889e4b59d6ae7f9
joohopark-tot <jooho.park@oberoi-is.net>
2026-02-12 21:35:13 +0530

    Update everton.js

--- team/everton.js
+++ team/everton.js
@@ -4,33 +4,32 @@ const everton = {
 
     { name:"Jordan Pickford", club:"Everton", league:"Prem", nationality:"England", position:"GK", age:31 },
     { name:"Mark Travers", club:"Everton", league:"Prem", nationality:"Ireland", position:"GK", age:26 },
-    { name:"Tom King", club:"Everton", league:"Prem", nationality:"Wales / England", position:"GK", age:30 },
+    { name:"Tom King", club:"Everton", league:"Prem", nationality:"Wales", position:"GK", age:30 },
 
     { name:"Jarrad Branthwaite", club:"Everton", league:"Prem", nationality:"England", position:"DF", age:23 },
-    { name:"James Tarkowski", club:"Everton", league:"Prem", nationality:"England / Poland", position:"DF", age:33 },
-    { name:"Michael Keane", club:"Everton", league:"Prem", nationality:"England / Ireland", position:"DF", age:33 },
+    { name:"James Tarkowski", club:"Everton", league:"Prem", nationality:"England", position:"DF", age:33 },
+    { name:"Michael Keane", club:"Everton", league:"Prem", nationality:"England", position:"DF", age:33 },
     { name:"Vitaliy Mykolenko", club:"Everton", league:"Prem", nationality:"Ukraine", position:"DF", age:26 },
-    { name:"Adam Aznou", club:"Everton", league:"Prem", nationality:"Morocco / Spain", position:"DF", age:19 },
+    { name:"Adam Aznou", club:"Everton", league:"Prem", nationality:"Morocco", position:"DF", age:19 },
     { name:"Jake O'Brien", club:"Everton", league:"Prem", nationality:"Ireland", position:"DF", age:24 },
     { name:"Nathan Patterson", club:"Everton", league:"Prem", nationality:"Scotland", position:"DF", age:24 },
     { name:"Séamus Coleman", club:"Everton", league:"Prem", nationality:"Ireland", position:"DF", age:37 },
 
     { name:"James Garner", club:"Everton", league:"Prem", nationality:"England", position:"MF", age:24 },
     { name:"Idrissa Gueye", club:"Everton", league:"Prem", nationality:"Senegal", position:"MF", age:36 },
-    { name:"Kiernan Dewsbury-Hall", club:"Everton", league:"Prem", nationality:"England / Ireland", position:"MF", age:27 },
+    { name:"Kiernan Dewsbury-Hall", club:"Everton", league:"Prem", nationality:"England", position:"MF", age:27 },
     { name:"Carlos Alcaraz", club:"Everton", league:"Prem", nationality:"Argentina", position:"MF", age:23 },
-    { name:"Tim Iroegbunam", club:"Everton", league:"Prem", nationality:"England / Nigeria", position:"MF", age:22 },
+    { name:"Tim Iroegbunam", club:"Everton", league:"Prem", nationality:"England", position:"MF", age:22 },
     { name:"Harrison Armstrong", club:"Everton", league:"Prem", nationality:"England", position:"MF", age:19 },
     { name:"Merlin Röhl", club:"Everton", league:"Prem", nationality:"Germany", position:"MF", age:23 },
 
-    { name:"Jack Grealish", club:"Everton", league:"Prem", nationality:"England / Ireland", position:"FW", age:30 },
-    { name:"Tyrique George", club:"Everton", league:"Prem", nationality:"England / Nigeria", position:"FW", age:20 },
-    { name:"Dwight McNeil", club:"Everton", league:"Prem", nationality:"England / Jamaica", position:"FW", age:26 },
-    { name:"Iliman Ndiaye", club:"Everton", league:"Prem", nationality:"Senegal / France", position:"FW", age:25 },
+    { name:"Jack Grealish", club:"Everton", league:"Prem", nationality:"England", position:"FW", age:30 },
+    { name:"Tyrique George", club:"Everton", league:"Prem", nationality:"England", position:"FW", age:20 },
+    { name:"Dwight McNeil", club:"Everton", league:"Prem", nationality:"England", position:"FW", age:26 },
+    { name:"Iliman Ndiaye", club:"Everton", league:"Prem", nationality:"Senegal", position:"FW", age:25 },
     { name:"Tyler Dibling", club:"Everton", league:"Prem", nationality:"England", position:"FW", age:19 },
-    { name:"Thierno Barry", club:"Everton", league:"Prem", nationality:"France / Guinea", position:"FW", age:23 },
-    { name:"Beto", club:"Everton", league:"Prem", nationality:"Guinea-Bissau / Portugal", position:"FW", age:28 }
+    { name:"Thierno Barry", club:"Everton", league:"Prem", nationality:"France", position:"FW", age:23 },
+    { name:"Beto", club:"Everton", league:"Prem", nationality:"Guinea-Bissau", position:"FW", age:28 }
 
   ]
 };
-
