96540c0870d1aa3f6d2cdeafaa73f5c1bcf9dd72
joohopark-tot <jooho.park@oberoi-is.net>
2026-02-12 10:52:22 +0530

    Update chelsea.js

--- team/chelsea.js
+++ team/chelsea.js
@@ -1,40 +1,35 @@
-// chelseaSquad.js
+const chelsea = [
 
-const chelseaSquad = [
-  { name:"Robert Sánchez", club:"Chelsea", league:"Prem", nationality:"Spain", position:"GK", age:27 },
-  { name:"Filip Jørgensen", club:"Chelsea", league:"Prem", nationality:"Denmark", position:"GK", age:23 },
-  { name:"Teddy Sharman-Lowe", club:"Chelsea", league:"Prem", nationality:"England", position:"GK", age:22 },
-  { name:"Gaga Slonina", club:"Chelsea", league:"Prem", nationality:"USA", position:"GK", age:21 },
+/* --- CHELSEA 2025/26 (Post-Winter) --- */
 
-  { name:"Reece James", club:"Chelsea", league:"Prem", nationality:"England", position:"DF", age:25 },
-  { name:"Malo Gusto", club:"Chelsea", league:"Prem", nationality:"France", position:"DF", age:22 },
-  { name:"Wesley Fofana", club:"Chelsea", league:"Prem", nationality:"France", position:"DF", age:24 },
-  { name:"Levi Colwill", club:"Chelsea", league:"Prem", nationality:"England", position:"DF", age:22 },
-  { name:"Benoît Badiashile", club:"Chelsea", league:"Prem", nationality:"France", position:"DF", age:24 },
-  { name:"Tosin Adarabioyo", club:"Chelsea", league:"Prem", nationality:"England", position:"DF", age:27 },
-  { name:"Trevoh Chalobah", club:"Chelsea", league:"Prem", nationality:"England", position:"DF", age:26 },
-  { name:"Jorrel Hato", club:"Chelsea", league:"Prem", nationality:"Netherlands", position:"DF", age:19 },
-  { name:"Marc Cucurella", club:"Chelsea", league:"Prem", nationality:"Spain", position:"DF", age:27 },
-  { name:"Caleb Wiley", club:"Chelsea", league:"Prem", nationality:"USA", position:"DF", age:20 },
-  { name:"Mamadou Sarr", club:"Chelsea", league:"Prem", nationality:"France", position:"DF", age:20 },
+{ name:"Robert Sánchez", club:"Chelsea", league:"Prem", nationality:"Spain", position:"GK", age:27 },
+{ name:"Filip Jørgensen", club:"Chelsea", league:"Prem", nationality:"Denmark", position:"GK", age:23 },
+{ name:"Gabriel Slonina", club:"Chelsea", league:"Prem", nationality:"USA", position:"GK", age:21 },
 
-  { name:"Moisés Caicedo", club:"Chelsea", league:"Prem", nationality:"Ecuador", position:"MF", age:23 },
-  { name:"Romeo Lavia", club:"Chelsea", league:"Prem", nationality:"Belgium", position:"MF", age:21 },
-  { name:"Enzo Fernández", club:"Chelsea", league:"Prem", nationality:"Argentina", position:"MF", age:24 },
-  { name:"Kiernan Dewsbury-Hall", club:"Chelsea", league:"Prem", nationality:"England", position:"MF", age:26 },
-  { name:"Cole Palmer", club:"Chelsea", league:"Prem", nationality:"England", position:"MF", age:23 },
-  { name:"Dário Essugo", club:"Chelsea", league:"Prem", nationality:"Portugal", position:"MF", age:20 },
+{ name:"Reece James", club:"Chelsea", league:"Prem", nationality:"England", position:"DF", age:26 },
+{ name:"Levi Colwill", club:"Chelsea", league:"Prem", nationality:"England", position:"DF", age:22 },
+{ name:"Wesley Fofana", club:"Chelsea", league:"Prem", nationality:"France", position:"DF", age:24 },
+{ name:"Marc Cucurella", club:"Chelsea", league:"Prem", nationality:"Spain", position:"DF", age:27 },
+{ name:"Malo Gusto", club:"Chelsea", league:"Prem", nationality:"France", position:"DF", age:22 },
+{ name:"Trevoh Chalobah", club:"Chelsea", league:"Prem", nationality:"England", position:"DF", age:26 },
+{ name:"Tosin Adarabioyo", club:"Chelsea", league:"Prem", nationality:"England", position:"DF", age:28 },
+{ name:"Jorrel Hato", club:"Chelsea", league:"Prem", nationality:"Netherlands", position:"DF", age:19 },
+{ name:"Josh Acheampong", club:"Chelsea", league:"Prem", nationality:"England", position:"DF", age:19 },
 
-  { name:"Pedro Neto", club:"Chelsea", league:"Prem", nationality:"Portugal", position:"FW", age:25 },
-  { name:"Noni Madueke", club:"Chelsea", league:"Prem", nationality:"England", position:"FW", age:23 },
-  { name:"Mykhailo Mudryk", club:"Chelsea", league:"Prem", nationality:"Ukraine", position:"FW", age:24 },
-  { name:"Jamie Gittens", club:"Chelsea", league:"Prem", nationality:"England", position:"FW", age:21 },
-  { name:"Alejandro Garnacho", club:"Chelsea", league:"Prem", nationality:"Argentina", position:"FW", age:21 },
-  { name:"Christopher Nkunku", club:"Chelsea", league:"Prem", nationality:"France", position:"FW", age:27 },
-  { name:"Nicolas Jackson", club:"Chelsea", league:"Prem", nationality:"Senegal", position:"FW", age:24 },
-  { name:"João Pedro", club:"Chelsea", league:"Prem", nationality:"Brazil", position:"FW", age:24 },
-  { name:"Liam Delap", club:"Chelsea", league:"Prem", nationality:"England", position:"FW", age:22 },
-  { name:"Estêvão", club:"Chelsea", league:"Prem", nationality:"Brazil", position:"FW", age:18 }
+{ name:"Moisés Caicedo", club:"Chelsea", league:"Prem", nationality:"Ecuador", position:"MF", age:24 },
+{ name:"Enzo Fernández", club:"Chelsea", league:"Prem", nationality:"Argentina", position:"MF", age:25 },
+{ name:"Romeo Lavia", club:"Chelsea", league:"Prem", nationality:"Belgium", position:"MF", age:21 },
+{ name:"Andrey Santos", club:"Chelsea", league:"Prem", nationality:"Brazil", position:"MF", age:21 },
+{ name:"Dário Essugo", club:"Chelsea", league:"Prem", nationality:"Portugal", position:"MF", age:20 },
+{ name:"Cole Palmer", club:"Chelsea", league:"Prem", nationality:"England", position:"MF", age:23 },
+
+{ name:"Pedro Neto", club:"Chelsea", league:"Prem", nationality:"Portugal", position:"FW", age:25 },
+{ name:"Estêvão", club:"Chelsea", league:"Prem", nationality:"Brazil", position:"FW", age:18 },
+{ name:"João Pedro", club:"Chelsea", league:"Prem", nationality:"Brazil", position:"FW", age:24 },
+{ name:"Liam Delap", club:"Chelsea", league:"Prem", nationality:"England", position:"FW", age:22 },
+{ name:"Alejandro Garnacho", club:"Chelsea", league:"Prem", nationality:"Argentina", position:"FW", age:21 },
+{ name:"Jamie Gittens", club:"Chelsea", league:"Prem", nationality:"England", position:"FW", age:21 }
+
 ];
 
-export default chelseaSquad;
+export default chelsea;
