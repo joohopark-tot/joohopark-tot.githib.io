dfbfc86033e6bbf17fe32dae338f49f80feef740
joohopark-tot <jooho.park@oberoi-is.net>
2026-02-12 21:44:20 +0530

    Update nottingham.js

--- team/nottingham.js
+++ team/nottingham.js
@@ -1,1 +1,40 @@
+const nottinghamForest = {
+  name: "Nottingham Forest",
+  players: [
+
+    { name:"John Victor", club:"Nottingham Forest", league:"Prem", nationality:"Brazil", position:"GK", age:29 },
+    { name:"Matz Sels", club:"Nottingham Forest", league:"Prem", nationality:"Belgium", position:"GK", age:33 },
+    { name:"Stefan Ortega", club:"Nottingham Forest", league:"Prem", nationality:"Germany", position:"GK", age:33 },
+    { name:"Angus Gunn", club:"Nottingham Forest", league:"Prem", nationality:"Scotland", position:"GK", age:30 },
+
+    { name:"Murillo", club:"Nottingham Forest", league:"Prem", nationality:"Brazil", position:"DF", age:23 },
+    { name:"Nikola Milenković", club:"Nottingham Forest", league:"Prem", nationality:"Serbia", position:"DF", age:28 },
+    { name:"Morato", club:"Nottingham Forest", league:"Prem", nationality:"Brazil", position:"DF", age:24 },
+    { name:"Jair Cunha", club:"Nottingham Forest", league:"Prem", nationality:"Brazil", position:"DF", age:20 },
+    { name:"Willy Boly", club:"Nottingham Forest", league:"Prem", nationality:"Ivory Coast", position:"DF", age:35 },
+    { name:"Zach Abbott", club:"Nottingham Forest", league:"Prem", nationality:"England", position:"DF", age:19 },
+    { name:"Neco Williams", club:"Nottingham Forest", league:"Prem", nationality:"Wales", position:"DF", age:24 },
+    { name:"Luca Netz", club:"Nottingham Forest", league:"Prem", nationality:"Germany", position:"DF", age:22 },
+    { name:"Ola Aina", club:"Nottingham Forest", league:"Prem", nationality:"Nigeria", position:"DF", age:29 },
+    { name:"Nicolò Savona", club:"Nottingham Forest", league:"Prem", nationality:"Italy", position:"DF", age:22 },
+    { name:"Eric da Silva Moreira", club:"Nottingham Forest", league:"Prem", nationality:"Germany", position:"DF", age:19 },
+
+    { name:"Ibrahim Sangaré", club:"Nottingham Forest", league:"Prem", nationality:"Ivory Coast", position:"MF", age:28 },
+    { name:"Elliot Anderson", club:"Nottingham Forest", league:"Prem", nationality:"England", position:"MF", age:23 },
+    { name:"Nicolás Domínguez", club:"Nottingham Forest", league:"Prem", nationality:"Argentina", position:"MF", age:27 },
+    { name:"Ryan Yates", club:"Nottingham Forest", league:"Prem", nationality:"England", position:"MF", age:28 },
+    { name:"Morgan Gibbs-White", club:"Nottingham Forest", league:"Prem", nationality:"England", position:"MF", age:26 },
+    { name:"Omari Hutchinson", club:"Nottingham Forest", league:"Prem", nationality:"England", position:"MF", age:22 },
+    { name:"James McAtee", club:"Nottingham Forest", league:"Prem", nationality:"England", position:"MF", age:23 },
+
+    { name:"Callum Hudson-Odoi", club:"Nottingham Forest", league:"Prem", nationality:"England", position:"FW", age:25 },
+    { name:"Dan Ndoye", club:"Nottingham Forest", league:"Prem", nationality:"Switzerland", position:"FW", age:25 },
+    { name:"Dilane Bakwa", club:"Nottingham Forest", league:"Prem", nationality:"France", position:"FW", age:23 },
+    { name:"Lorenzo Lucca", club:"Nottingham Forest", league:"Prem", nationality:"Italy", position:"FW", age:25 },
+    { name:"Igor Jesus", club:"Nottingham Forest", league:"Prem", nationality:"Brazil", position:"FW", age:24 },
+    { name:"Taiwo Awoniyi", club:"Nottingham Forest", league:"Prem", nationality:"Nigeria", position:"FW", age:28 },
+    { name:"Chris Wood", club:"Nottingham Forest", league:"Prem", nationality:"New Zealand", position:"FW", age:34 }
+
+  ]
+};
 
