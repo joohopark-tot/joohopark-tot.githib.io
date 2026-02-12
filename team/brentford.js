c4d2ba0eb50b051d94ee4d3bbf874018d7278798
joohopark-tot <jooho.park@oberoi-is.net>
2026-02-12 21:20:18 +0530

    Update brentford.js

--- team/brentford.js
+++ team/brentford.js
@@ -1,1 +1,37 @@
+const brentford = {
+  name: "Brentford",
+  players: [
+
+    { name:"Caoimhín Kelleher", club:"Brentford", league:"Prem", nationality:"Ireland", position:"GK", age:27 },
+    { name:"Hákon Valdimarsson", club:"Brentford", league:"Prem", nationality:"Iceland", position:"GK", age:24 },
+    { name:"Ellery Balcombe", club:"Brentford", league:"Prem", nationality:"England", position:"GK", age:26 },
+    { name:"Julian Eyestone", club:"Brentford", league:"Prem", nationality:"United States", position:"GK", age:19 },
+
+    { name:"Nathan Collins", club:"Brentford", league:"Prem", nationality:"Ireland", position:"DF", age:24 },
+    { name:"Sepp van den Berg", club:"Brentford", league:"Prem", nationality:"Netherlands", position:"DF", age:24 },
+    { name:"Kristoffer Ajer", club:"Brentford", league:"Prem", nationality:"Norway", position:"DF", age:27 },
+    { name:"Ethan Pinnock", club:"Brentford", league:"Prem", nationality:"Jamaica", position:"DF", age:32 },
+    { name:"Rico Henry", club:"Brentford", league:"Prem", nationality:"Jamaica", position:"DF", age:28 },
+    { name:"Michael Kayode", club:"Brentford", league:"Prem", nationality:"Italy", position:"DF", age:21 },
+    { name:"Aaron Hickey", club:"Brentford", league:"Prem", nationality:"Scotland", position:"DF", age:23 },
+
+    { name:"Vitaly Janelt", club:"Brentford", league:"Prem", nationality:"Germany", position:"MF", age:27 },
+    { name:"Jordan Henderson", club:"Brentford", league:"Prem", nationality:"England", position:"MF", age:35 },
+    { name:"Yegor Yarmolyuk", club:"Brentford", league:"Prem", nationality:"Ukraine", position:"MF", age:21 },
+    { name:"Mathias Jensen", club:"Brentford", league:"Prem", nationality:"Denmark", position:"MF", age:30 },
+    { name:"Josh Dasilva", club:"Brentford", league:"Prem", nationality:"England", position:"MF", age:27 },
+    { name:"Keane Lewis-Potter", club:"Brentford", league:"Prem", nationality:"England", position:"MF", age:24 },
+    { name:"Mikkel Damsgaard", club:"Brentford", league:"Prem", nationality:"Denmark", position:"MF", age:25 },
+    { name:"Antoni Milambo", club:"Brentford", league:"Prem", nationality:"Netherlands", position:"MF", age:20 },
+    { name:"Fábio Carvalho", club:"Brentford", league:"Prem", nationality:"Portugal", position:"MF", age:23 },
+
+    { name:"Kevin Schade", club:"Brentford", league:"Prem", nationality:"Germany", position:"FW", age:24 },
+    { name:"Reiss Nelson", club:"Brentford", league:"Prem", nationality:"England", position:"FW", age:26 },
+    { name:"Dango Ouattara", club:"Brentford", league:"Prem", nationality:"Burkina Faso", position:"FW", age:24 },
+    { name:"Romelle Donovan", club:"Brentford", league:"Prem", nationality:"England", position:"FW", age:19 },
+    { name:"Igor Thiago", club:"Brentford", league:"Prem", nationality:"Brazil", position:"FW", age:24 },
+    { name:"Kaye Furo", club:"Brentford", league:"Prem", nationality:"Belgium", position:"FW", age:19 }
+
+  ]
+};
 
