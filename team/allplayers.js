dcc96bba93dde01f380988b1e2fb954a174f23a0
joohopark-tot <jooho.park@oberoi-is.net>
2026-02-13 10:12:07 +0530

    Update allplayers.js

--- team/allplayers.js
+++ team/allplayers.js
@@ -1,23 +1,23 @@
-import { arsenal } from "./team/arsenal.js";
-import { astonvilla } from "./team/astonvilla.js";
-import { bournemouth } from "./team/bournemouth.js";
-import { brentford } from "./team/brentford.js";
-import { brighton } from "./team/brighton.js";
-import { burnley } from "./team/burnley.js";
-import { chelsea } from "./team/chelsea.js";
-import { everton } from "./team/everton.js";
-import { fulham } from "./team/fulham.js";
-import { leeds } from "./team/leeds.js";
-import { liverpool } from "./team/liverpool.js";
-import { mancity } from "./team/mancity.js";
-import { manutd } from "./team/manutd.js";
-import { newcastle } from "./team/newcastle.js";
-import { nottingham } from "./team/nottingham.js";
-import { palace } from "./team/palace.js";
-import { sunderland } from "./team/sunderland.js";
-import { tottenham } from "./team/tottenham.js";
-import { westham } from "./team/westham.js";
-import { wolves } from "./team/wolves.js";
+import { arsenal } from "./arsenal.js";
+import { astonvilla } from "./astonvilla.js";
+import { bournemouth } from "./bournemouth.js";
+import { brentford } from "./brentford.js";
+import { brighton } from "./brighton.js";
+import { burnley } from "./burnley.js";
+import { chelsea } from "./chelsea.js";
+import { everton } from "./everton.js";
+import { fulham } from "./fulham.js";
+import { leeds } from "./leeds.js";
+import { liverpool } from "./liverpool.js";
+import { mancity } from "./mancity.js";
+import { manutd } from "./manutd.js";
+import { newcastle } from "./newcastle.js";
+import { nottingham } from "./nottingham.js";
+import { palace } from "./palace.js";
+import { sunderland } from "./sunderland.js";
+import { tottenham } from "./tottenham.js";
+import { westham } from "./westham.js";
+import { wolves } from "./wolves.js";
 
 export default [
   ...arsenal,
